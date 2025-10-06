921f8bf0ba4387540708b0f32e99e4ad0319405d
Behaylu gule <62138994+behaylugule@users.noreply.github.com>
2025-10-06 19:21:00 +0300

    Update asgi.py

--- ragchat/ragchat/asgi.py
+++ ragchat/ragchat/asgi.py
@@ -3,9 +3,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ragchat.settings")
-
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
